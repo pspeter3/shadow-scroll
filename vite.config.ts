d06c32798a9fb048d9061886bbc9ed58e38cb54b
Phips Peter <phips@asana.com>
2025-04-10 10:17:20 -0400

    Add Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+/// <reference types="vitest/config" />
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    !process.env.VITEST && reactRouter(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+  test: {
+    environment: "happy-dom",
+    setupFiles: ["./app/utils/setup.ts"],
+  },
 });
