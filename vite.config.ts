a19bdbda851de0d5bf8455d114c410c255ce22a5
Phips Peter <phips@asana.com>
2025-04-10 10:18:30 -0400

    Add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { coverageConfigDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,21 @@ export default defineConfig({
     tsconfigPaths(),
   ],
   test: {
+    coverage: {
+      enabled: true,
+      exclude: coverageConfigDefaults.exclude.concat([
+        "*.config.ts",
+        "build/**/*.js",
+        "app/routes.ts",
+        "app/root.tsx",
+      ]),
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
     environment: "happy-dom",
     setupFiles: ["./app/utils/setup.ts"],
   },
